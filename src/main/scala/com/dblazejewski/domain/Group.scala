fb4bf527a7116991b63d7e6683110f6c7208c359
damian <damian@withlocals.com>
2019-03-11 01:04:27 +0100

    fix formatting

--- src/main/scala/com/dblazejewski/domain/Group.scala
+++ src/main/scala/com/dblazejewski/domain/Group.scala
@@ -4,7 +4,4 @@ final case class Group(id: Option[Int], name: String) extends IdEntity
 
 final case class Groups(groups: Seq[Group])
 
-final case class UserParticipatesInGroup(
-  id: Option[Int],
-  userId: Int,
-  groupId: Int) extends IdEntity
+final case class UserParticipatesInGroup(id: Option[Int], userId: Int, groupId: Int) extends IdEntity
--- src/main/scala/com/dblazejewski/domain/Post.scala
+++ src/main/scala/com/dblazejewski/domain/Post.scala
@@ -2,9 +2,8 @@ package com.dblazejewski.domain
 
 import java.time.LocalDateTime
 
-case class Post(
-  id: Option[Int],
-  authorId: Int,
-  groupId: Int,
-  createdAt: LocalDateTime,
-  content: String) extends IdEntity
+case class Post(id: Option[Int],
+                authorId: Int,
+                groupId: Int,
+                createdAt: LocalDateTime,
+                content: String) extends IdEntity
