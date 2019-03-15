babacda98091a5a58297eeb8cc37ba6fe609c40d
damian <damian@withlocals.com>
2019-03-15 17:40:12 +0100

    add authorization

--- src/main/scala/com/dblazejewski/api/FeedRoutes.scala
+++ src/main/scala/com/dblazejewski/api/FeedRoutes.scala
@@ -9,11 +9,13 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
 import akka.pattern.ask
 import akka.util.Timeout
+import com.dblazejewski.api.support.AuthorizationSupport
 import com.dblazejewski.application.FeedActor._
 import com.dblazejewski.domain.PostWithAuthor
 import com.dblazejewski.support.JsonSupport
 import com.typesafe.scalalogging.StrictLogging
 import com.dblazejewski.support.UuidSupport._
+
 import scala.concurrent.duration._
 
 final case class GroupFeedResponse(groupId: UUID, feed: Seq[PostWithAuthor])
@@ -21,6 +23,7 @@ final case class GroupFeedResponse(groupId: UUID, feed: Seq[PostWithAuthor])
 final case class UserFeedResponse(userId: UUID, feed: Seq[UserFeedItem])
 
 trait FeedRoutes extends JsonSupport
+  with AuthorizationSupport
   with StrictLogging {
 
   implicit def system: ActorSystem
@@ -30,29 +33,33 @@ trait FeedRoutes extends JsonSupport
   private implicit lazy val timeout: Timeout = Timeout(5 seconds)
 
   lazy val feedRoutes: Route =
-    pathPrefix("feed") {
-      pathPrefix("group" / Segment) { groupIdParam =>
-        get {
-          onSuccess(feedActor ? GetGroupFeed(getUUID(groupIdParam))) {
-            case ReturnGroupFeed(groupId, feed) =>
-              complete(StatusCodes.OK, GroupFeedResponse(groupId, feed))
-            case error: GetGroupFeedFailed =>
-              logger.error(s"Error fetching group [${error.groupId}] feed", error.msg)
-              complete(StatusCodes.NotFound, error)
-          }
-        }
-
-      } ~
-        pathPrefix("all" / Segment) { userIdParam =>
-          get {
-            onSuccess(feedActor ? GetUserFeed(getUUID(userIdParam))) {
-              case ReturnUserFeed(userId, feed) =>
-                complete(StatusCodes.OK, UserFeedResponse(userId, feed))
-              case error: GetUserFeedFailed =>
-                logger.error(s"Error fetching user [${error.userId}] feed", error.msg)
-                complete(StatusCodes.InternalServerError, error)
+    extractCredentials { credentials =>
+      authorizeAsync(_ => hasValidToken(tokenAuthenticator(credentials))) {
+        pathPrefix("feed") {
+          pathPrefix("group" / Segment) { groupIdParam =>
+            get {
+              onSuccess(feedActor ? GetGroupFeed(getUUID(groupIdParam))) {
+                case ReturnGroupFeed(groupId, feed) =>
+                  complete(StatusCodes.OK, GroupFeedResponse(groupId, feed))
+                case error: GetGroupFeedFailed =>
+                  logger.error(s"Error fetching group [${error.groupId}] feed", error.msg)
+                  complete(StatusCodes.NotFound, error)
+              }
             }
-          }
+
+          } ~
+            pathPrefix("all" / Segment) { userIdParam =>
+              get {
+                onSuccess(feedActor ? GetUserFeed(getUUID(userIdParam))) {
+                  case ReturnUserFeed(userId, feed) =>
+                    complete(StatusCodes.OK, UserFeedResponse(userId, feed))
+                  case error: GetUserFeedFailed =>
+                    logger.error(s"Error fetching user [${error.userId}] feed", error.msg)
+                    complete(StatusCodes.InternalServerError, error)
+                }
+              }
+            }
         }
+      }
     }
 }
--- src/main/scala/com/dblazejewski/api/UserRoutes.scala
+++ src/main/scala/com/dblazejewski/api/UserRoutes.scala
@@ -9,6 +9,7 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
 import akka.pattern.ask
 import akka.util.Timeout
+import com.dblazejewski.api.support.AuthorizationSupport
 import com.dblazejewski.application.UserActor.{CreateUser, UserAddFailed, UserAdded}
 import com.dblazejewski.support.JsonSupport
 import com.typesafe.scalalogging.StrictLogging
@@ -22,6 +23,7 @@ final case class UserNameNotAdded(name: String, msg: String)
 final case class AddUserBody(name: String)
 
 trait UserRoutes extends JsonSupport
+  with AuthorizationSupport
   with StrictLogging {
 
   implicit def system: ActorSystem
@@ -31,16 +33,20 @@ trait UserRoutes extends JsonSupport
   private implicit lazy val timeout: Timeout = Timeout(5 seconds)
 
   lazy val userRoutes: Route =
-    pathPrefix("user") {
-      pathEnd {
-        post {
-          entity(as[AddUserBody]) { body =>
-            onSuccess(userActor ? CreateUser(body.name)) {
-              case UserAdded(id) =>
-                complete(StatusCodes.Created, UserIdAdded(id))
-              case UserAddFailed(name) =>
-                logger.error(s"Error adding user [$name]")
-                complete(StatusCodes.InternalServerError, UserNameNotAdded(name, "Error adding user"))
+    extractCredentials { credentials =>
+      authorizeAsync(_ => hasValidToken(tokenAuthenticator(credentials))) {
+        pathPrefix("user") {
+          pathEnd {
+            post {
+              entity(as[AddUserBody]) { body =>
+                onSuccess(userActor ? CreateUser(body.name)) {
+                  case UserAdded(id) =>
+                    complete(StatusCodes.Created, UserIdAdded(id))
+                  case UserAddFailed(name) =>
+                    logger.error(s"Error adding user [$name]")
+                    complete(StatusCodes.InternalServerError, UserNameNotAdded(name, "Error adding user"))
+                }
+              }
             }
           }
         }
